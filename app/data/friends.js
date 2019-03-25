67f4fb31191c9e7da652bf084d3950fe655ff2ad
Sara Overby <saraoverby@Mendys-Air.domain>
2019-03-24 21:11:55 -0600

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Michael Scott",
+		photo: "../images/michael.png",
+		scores: [
+            "5",
+            "1",
+			"1",
+			"1",
+			"1",
+			"5",
+			"3",
+			"1",
+			"5",
+			"5"
+		]
+	},
+	{
+		name: "Wall-E",
+		photo: "../images/walle.jpg",
+		scores: [
+            "3",
+			"4",
+			"2",
+			"3",
+			"5",
+			"3",
+			"5",
+			"2",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Denver Mayor Michael Hancock",
+		photo: "../images/hancock.jpg",
+		score: [
+            "3",
+			"3",
+			"4",
+			"4",
+			"2",
+			"2",
+			"3",
+			"5",
+			"2",
+			"3"		
+		]
+	}
+];
+
+module.exports = friends;
